43000943db071f57d41592e7de5c59a2b35df3a7
Sai-rohit17 <dsrohit2000@gmail.com>
2022-12-22 11:22:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  FacebookShareButton,
+  WhatsappShareButton,
+  WhatsappIcon,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  TelegramShareButton,
+  TelegramIcon,
+} from 'react-share';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const shareLink = "https://common-coredev-eks-wlpc.cloud.synchronoss.net/web/app/share/invite/bBUkTKUnmP"
+    const shareUrl = "https://sai-rohit17.github.io/cloud-preview/"
+    return (
+    <div>
+      <h1>share ur image</h1>
+      <FacebookShareButton
+      url={shareLink}
+      quote={'India is my country'}
+      hashtag={'#India'}
+      >
+        <FacebookIcon size={40} round={true} />
+      </FacebookShareButton>
+      <WhatsappShareButton
+      url={shareLink}
+      quote={'India is my country'}
+      hashtag={'#India'}
+      >
+        <WhatsappIcon size={40} round={true} />
+      </WhatsappShareButton>
+      <TwitterShareButton
+      url={shareUrl}
+      quote={'India is my country'}
+      hashtag={'India'}
+      >
+        <TwitterIcon size={40} round={true} />
+      </TwitterShareButton>
+      <TelegramShareButton
+      url={shareLink}
+      quote={'India is my country'}
+      >
+        <TelegramIcon size={40} round={true} />
+      </TelegramShareButton>
     </div>
   );
 }
