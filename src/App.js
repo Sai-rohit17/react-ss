845525bfa7ee5e8567576091209c898852a54436
Sai-rohit17 <dsrohit2000@gmail.com>
2022-12-30 20:52:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -41,10 +41,10 @@ function App() {
         <meta name="twitter:card" content="summary" />
 <meta name="twitter:site" content="@DuvvuriRohit" />
 <meta name="twitter:creator" content="@DuvvuriRohit" />
-<meta property="og:url" content={shareLink} />
+<meta property="og:url" content="https://sportshub.cbsistatic.com/i/2022/05/09/7e187fc9-2f31-4eb3-92ba-bf60e4fa35a1/avatar-2-teaser-trailer-images-jake-sulai-sam-worthington.jpg" />
 <meta property="og:title" content="A Twitter for My Sister" />
 <meta property="og:description" content="In the early days, Twitter grew so quickly that it was almost impossible to add new features because engineers spent their time trying to keep the rocket ship from stalling." />
-<meta property="og:image" content={shareLink} />
+<meta property="og:image" content="https://sportshub.cbsistatic.com/i/2022/05/09/7e187fc9-2f31-4eb3-92ba-bf60e4fa35a1/avatar-2-teaser-trailer-images-jake-sulai-sam-worthington.jpg" />
           </Helmet>
         <TwitterIcon size={40} round={true} />
       </TwitterShareButton>
